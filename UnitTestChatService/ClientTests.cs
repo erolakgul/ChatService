001a129565092aa76599d53130f19a68b5b9ba22
EROL AKGÜL <erolakgul88@gmail.com>
2022-09-23 20:44:07 +0300

    Add project files.

---
+++ UnitTestChatService/ClientTests.cs
@@ -0,0 +1,14 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace UnitTestChatService
+{
+    [TestClass]
+    public class ClientTests
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+
+        }
+    }
+}
