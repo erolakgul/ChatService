9e146fa7c7f5abc0e174b67d59b60ae231439692
EROL AKGÜL <erolakgul88@gmail.com>
2022-09-24 17:22:44 +0300

    created services interfaces on core layer

---
+++ chatService.core/Services/Basis/IErrorService.cs
@@ -0,0 +1,26 @@
+﻿using chatService.core.DTO;
+
+namespace chatService.core.Services.Basis
+{
+    /// <summary>
+    /// error service
+    /// </summary>
+    public interface IErrorService
+    {
+        /// <summary>
+        ///  adding method for error repository
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="guid"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        ErrorDto FillError(object key, Guid guid, ErrorDto entity);
+
+        /// <summary>
+        /// removing method for error repository
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="guid"></param>
+        void RemoveError(object key, Guid guid);
+    }
+}
---
+++ chatService.core/Services/Basis/IMessageService.cs
@@ -0,0 +1,26 @@
+﻿using chatService.core.DTO;
+
+namespace chatService.core.Services.Basis
+{
+    /// <summary>
+    /// message service
+    /// </summary>
+    public interface IMessageService
+    {
+        /// <summary>
+        ///  adding method for message repository
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="guid"></param>
+        /// <param name="entity"></param>
+        /// <returns></returns>
+        MessageDto FillMessage(object key, Guid guid, MessageDto entity);
+
+        /// <summary>
+        /// removing method for message repository
+        /// </summary>
+        /// <param name="key"></param>
+        /// <param name="guid"></param>
+        void RemoveMessage(object key, Guid guid);
+    }
+}
