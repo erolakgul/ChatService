56c4dce238a7bb088c90765341c7ad504a3c21e8
EROL AKGÜL <erolakgul88@gmail.com>
2022-09-24 17:35:42 +0300

    created unit of work interfaces on core layer

---
+++ chatService.core/UOW/IUnitOfWork.cs
@@ -0,0 +1,16 @@
+﻿using chatService.core.Repositories.Basis;
+
+namespace chatService.core.UOW
+{
+    /// <summary>
+    /// unit of work structer interface
+    /// </summary>
+    public interface IUnitOfWork
+    {
+        /// <summary>
+        /// all objects
+        /// </summary>
+        IErrorRepository ErrorRepository { get; }
+        IMessageRepository MessageRepository { get; }
+    }
+}
