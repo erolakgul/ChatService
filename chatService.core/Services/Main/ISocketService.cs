bea4fcc5bcd24dcb1881e6e07f96bca73285dce2
EROL AKGÜL <erolakgul88@gmail.com>
2022-09-25 23:28:29 +0300

    created socket service classes for their repository

---
+++ chatService.core/Services/Main/ISocketService.cs
@@ -0,0 +1,11 @@
+﻿using chatService.core.DTO;
+using System.Net;
+
+namespace chatService.core.Services.Main
+{
+    public interface ISocketService
+    {
+        void Start(IPEndPoint ipEndPoint);
+        void TransferData(MessageDto messageDto);
+    }
+}
---
+++ chatService.service/Bussiness/Main/SocketService.cs
@@ -0,0 +1,26 @@
+﻿using chatService.core.DTO;
+using chatService.core.Services.Main;
+using chatService.core.UOW;
+using System.Net;
+
+namespace chatService.service.Bussiness.Main
+{
+    public class SocketService : ISocketService
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        public SocketService(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public void Start(IPEndPoint ipEndPoint)
+        {
+            _unitOfWork.SocketRepository.Start(ipEndPoint);
+        }
+
+        public void TransferData(MessageDto messageDto)
+        {
+           _unitOfWork.SocketRepository.TransferData(messageDto);
+        }
+    }
+}
