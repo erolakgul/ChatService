ca9590d3a96361c5ec480898db3b15d673ba8a3c
EROL AKGÜL <erolakgul88@gmail.com>
2022-09-28 13:19:46 +0300

    created UNIT TEST for Listenerservice's localsessionid is empty, added app_test9 image to the server layer

--- UnitTestChatService/ServerTests.cs
+++ UnitTestChatService/ServerTests.cs
@@ -1,13 +1,42 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿using chatService.core.UOW;
+using chatService.service.Bussiness.Main;
+using chatService.startup.Configurations;
+using chatService.startup.Provider;
+using Microsoft.Extensions.DependencyInjection;
+using NUnit.Framework;
+using System;
 
 namespace UnitTestChatService
 {
-    [TestClass]
+    [TestFixture]
     public class ServerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        [Test]
+        public void Check_Server_LocalSessionID_IsEmpty_WhenExecuted()
         {
+            #region arrange
+            ConnectionSettings connectionSettings = new();
+            #endregion
+
+            #region act
+            string filePath = connectionSettings.GetLibraryPath();
+            connectionSettings = connectionSettings.ReadJsonFile(filePath);
+
+
+              #region get dependency injection instance
+              ServiceProvider serviceProvider = CustomServiceProvider.GetInstance().serviceProvider;
+              IUnitOfWork iUnitOfWork = serviceProvider.GetService<IUnitOfWork>();
+              #endregion
+              #region getting listener service for server
+              ListenerService listenerService = new(iUnitOfWork);
+              listenerService.Start(Convert.ToInt32(connectionSettings.PortNumber), connectionSettings.MaxCountQueue);
+            #endregion
+
+            #endregion
+
+            #region assert
+            Assert.AreNotEqual(listenerService.LocalSessionID, Guid.Empty);
+            #endregion
 
         }
     }
